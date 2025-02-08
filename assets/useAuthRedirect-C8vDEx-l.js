ac04acdb507e26fefad9a469bddcc78918ba5e83
Nicholas_Huang713 <nhuang713@gmail.com>
2025-02-09 04:05:28 +0900

    Updates

--- assets/useAuthRedirect-C8vDEx-l.js
+++
@@ -1,1 +0,0 @@
-import{q as e,as as a,r as i}from"./index-51lu4ILa.js";const u=()=>{const{isAuthenticated:t}=e(),s=a();i.useEffect(()=>{t||s("/login")},[t,s])};export{u};
---
+++ assets/useAuthRedirect-DScrkuXo.js
@@ -0,0 +1,1 @@
+import{q as e,as as a,r as i}from"./index-ZsyY4Dx_.js";const u=()=>{const{isAuthenticated:t}=e(),s=a();i.useEffect(()=>{t||s("/login")},[t,s])};export{u};
